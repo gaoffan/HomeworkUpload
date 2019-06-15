46ba01bf035ada86a80bbc179ca97d48192af005
goufaan <suzukaze.kouko@gmail.com>
2019-06-15 21:08:58 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HomeworkUpload
 一个作业在线提交平台。
+
+这是Levyy的后端，使用前后端分离的形式开发。项目的配置在application.properties
+
+# Deploy
+需要JRE以及MongoDB。
+### 开发环境
+将homeworkupload.isdev设置为true，前端地址写在homeworkupload.dev.fronturl里。
+### 生产环境
+将homeworkupload.isdev设置为false，默认下homeworkupload.fronturl留空表示不使用CORS，此时后端通过Nginx反代使用同域访问。
--- src/test/java/com/goufaan/homeworkupload/HomeworkUploadApplicationTests.java
+++ src/test/java/com/goufaan/homeworkupload/HomeworkUploadApplicationTests.java
@@ -1,5 +1,6 @@
 package com.goufaan.homeworkupload;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class HomeworkUploadApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
